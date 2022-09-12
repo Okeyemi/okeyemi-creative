b24afbd86162253edb2f95d4237cec43e8ed1421
okeyemi <okeyemitunde@gmail.com>
2022-09-12 14:39:43 +0100

    updated

--- src/views/AboutPage.vue
+++ src/views/AboutPage.vue
@@ -1,15 +1,28 @@
 
 <template>
-    <div class="back" style="height:700px, bg-image ;">
-      
-      <h1>Designed by Okeyemi Creative</h1>
-    
-      <p>Okeyemi Tunde is an enthusiast who loves to code. Willing to learn and explore the tech world. This is just a tip of the iceberg</p>
+<section class="flex bg">
+  <transition name="fade">
+  <div>  
+    <h1 class="mt-2 text-3xl text-center ">
+      I am a technology enthusiast; I love to implement visual elements that users see and interact with in a web application. 
+    </h1>
     </div>
+  </transition>
+    </section>
   </template>
   
   <style>
-div{
-  background-image:src/assets/images/image 1.jpg);
+.fade-enter-active,
+.fade-leave-active {
+    transition:all 1s ease;
+}
+
+.fade-enter-from{
+    opacity: 0;
+  transform:translateX(-100%);
+}
+.fade-leave-to{
+  opacity: 0;
+  transform:translateX(100%);
 }
   </style>
