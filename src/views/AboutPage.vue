e71bdca8d8a7685d397d0eeae5c2d418a8c622e6
okeyemi <okeyemitunde@gmail.com>
2022-09-12 15:05:59 +0100

    updated

--- src/views/AboutPage.vue
+++ src/views/AboutPage.vue
@@ -1,17 +1,75 @@
-
 <template>
-<section class="flex bg">
+  <div  class="flex flex-wrap w-full relative">
+  <div  class="w-full absolute" v-for="(color, index) in sliders" :key="color">
   <transition name="fade">
-  <div>  
-    <h1 class="mt-2 text-3xl text-center ">
-      I am a technology enthusiast; I love to implement visual elements that users see and interact with in a web application. 
-    </h1>
+  <div v-if="currentSlide ==index" :class="color" style="height:350px">
+  <h1 class="text-center text-white text-4xl font-mono  font-boldp-2 p-2 ">Hi, My Name is Okeyemi Tunde</h1>
+  <p class="text-center text-white text-4xl font-mono  font-boldp-2 mt-2 p-2">I am a technology enthusiast; I love to implement visual elements that users see and interact with in a web application. </p>
+</div>
+</transition> 
+  </div>
+  <div class="w-full" style="height:340px">
+  <div class="absolute bottom-0 w-full flex justify-center">
+    <div v-for="slider in sliders"
+  :key="slider"
+  @click="makeActive(index)"
+  :class="currentSlide == index ? 'bg-gray-700' : 'bg-gray-300'"
+      class="w-4 mx-2 h-4 rounded-full cursor-pointer"> 
     </div>
-  </transition>
-    </section>
-  </template>
+  </div>
+   </div>
+  <!-- <div class="my-10 flex w-full">
+<div class="m-auto">
+        <h1 v-if="isTitleShowing">Slider Carousel</h1>
+        <button
+        @click="isTitleShowing= !isTitleShowing"
+         class="px-2 rounded border"
+         >
+         Toggle Text
+         </button>
+</div> -->
+  <!-- </div> -->
   
-  <style>
+</div>
+
+</template>
+
+<script>
+
+export default {
+data(){
+    return{
+        currentSlide: 1,
+        sliders:['bg-teal-600', 'bg-blue-600', 'bg-yellow-600'],
+        internal:'',
+        isTitleShowing:true,
+    };
+},
+methods:{
+  makeActive(index){
+    this.currentSlide=index;
+  },
+},
+mounted(){
+    this.currentSlide=setInterval(() => {
+        this.currentSlide= this.currentSlide==2 ? 0: this.currentSlide +1;
+    },5000);
+    // }, interval);
+    // setInterval(()=>{
+    //     if(this.currentSlide==2){
+    //         this.currentSlide=0;
+    //     }else{ 
+    //         this.currentSlide++;
+    //         }
+    // },2000);
+},
+beforeUnmount(){
+    clearInterval(this.internal);
+},
+};
+</script>
+
+<style>
 .fade-enter-active,
 .fade-leave-active {
     transition:all 1s ease;
@@ -25,4 +83,4 @@
   opacity: 0;
   transform:translateX(100%);
 }
-  </style>
+</style>
